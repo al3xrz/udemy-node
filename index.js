4f0b57a8265165b0225bc5c044be7a17162e6965
al3xrz <al3xrz@gmail.com>
2020-04-24 11:43:15 +0300

    handlebars + partials

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const app = express();
 const path =require('path')
 const exphbs = require('express-handlebars')
 
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
 
 const hbs = exphbs.create({
     extname: 'hbs', // расширение файла
-    defaultLayout: 'main'
+    defaultLayout: 'main' // main.hbs in views/layouts - точка входа
 })
 
 app.engine('hbs', hbs.engine); 
 app.set('view engine', 'hbs') // регистрация движка
-app.set('views', 'views')
+app.set('views', 'views') // папка с вьюхами
 
-app.get('/', (req, res)=>{
-    res.render('index')
-})
+app.use(express.static('public'))
+app.use(homeRoutes)
+app.use(addRoutes)
+app.use(coursesRoutes)
 
-app.get('/about', (req, res)=>{
-    res.status = 200;
-    res.sendFile(path.join(__dirname, 'views','about.html'))
 
-})
+
 
 
 
